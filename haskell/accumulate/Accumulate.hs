1a54b7e584e808501517fc03a94cd37ae0b1e2be
Stephen Karger <stephen@bridj.com>
2016-05-28 22:06:45 -0400

    simplify accumulate function

--- haskell/accumulate/Accumulate.hs
+++ haskell/accumulate/Accumulate.hs
@@ -1,8 +1,5 @@
 module Accumulate where
 
-myMap :: (a->b) -> [a] -> [b]
-myMap _ []       = []
-myMap f (x:xs)   = f x : myMap f xs
-
 accumulate :: (a -> b) -> [a] -> [b]
-accumulate fn elems = myMap fn elems 
+accumulate _  []         = []
+accumulate fn (el:elems) = fn el : accumulate fn elems
