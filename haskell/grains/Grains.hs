802172b7dfc3e28762df3eb1092ecf72a061288d
Stephen Karger <stephen@bridj.com>
2016-05-30 17:14:21 -0400

    input validation for square function

--- haskell/grains/Grains.hs
+++ haskell/grains/Grains.hs
@@ -1,7 +1,10 @@
 module Grains where
 
 square :: Int -> Integer
-square n = 2^(n-1)
+square n
+  | n < 1 || n > 64 = error "Invalid chess board square.\
+                            \ Must be in [1..64]"
+  | otherwise = 2^(n-1)
 
 total :: Integer
 total = 18446744073709551615
