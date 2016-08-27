4d17a4d0e18d2b50a545880986bc9476492ee15d
Stephen Karger <stephen@bridj.com>
2016-08-27 18:11:21 -0400

    concise with lambda

--- haskell/sum-of-multiples/src/SumOfMultiples.hs
+++ haskell/sum-of-multiples/src/SumOfMultiples.hs
@@ -2,12 +2,10 @@ module SumOfMultiples (sumOfMultiples) where
 import Data.List (sort, nub)
 
 sumOfMultiples :: (Integral a) => [a] -> a -> a
-sumOfMultiples numberSet upperBound =
-        sum . nub $ concat $ createMultiples numberSet upperBound
+sumOfMultiples factors upperBound =
+        sum . nub $ concat $ createMultiples factors upperBound
 
 createMultiples :: (Integral a) => [a] -> a -> [[a]]
-createMultiples factors upperBound = map (multiplesOf upperBound) factors
-
-multiplesOf :: (Integral a) => a -> a -> [a]
-multiplesOf upperBound factor = takeWhile (< upperBound) $ map (factor*) [1..]
-
+createMultiples factors upperBound = map
+        ((\ub f -> takeWhile (< ub) $ map (f*) [1..]) upperBound)
+        factors
