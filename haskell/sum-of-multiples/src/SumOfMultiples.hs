a96a4f5bdc009a690aefb0f42f3b3eabce7d1542
Stephen Karger <stephen@bridj.com>
2016-08-27 17:29:35 -0400

    SumOfMultiples

--- haskell/sum-of-multiples/src/SumOfMultiples.hs
+++ haskell/sum-of-multiples/src/SumOfMultiples.hs
@@ -1,3 +1,13 @@
 module SumOfMultiples (sumOfMultiples) where
+import Data.List (sort, nub)
 
-sumOfMultiples = undefined
+sumOfMultiples :: (Integral a) => [a] -> a -> a
+sumOfMultiples numberSet upperBound =
+        sum . nub $ concat $ createMultiples numberSet upperBound
+
+createMultiples :: (Integral a) => [a] -> a -> [[a]]
+createMultiples factors upperBound = map (multiplesOf upperBound) factors
+
+multiplesOf :: (Integral a) => a -> a -> [a]
+multiplesOf upperBound factor = takeWhile (< upperBound) $ map (factor*) [1..]
+
