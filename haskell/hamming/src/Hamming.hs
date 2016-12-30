12e5d14234fe44af0cad545a9c9ad2e0086cc9e9
Stephen Karger <stephen@bridj.com>
2016-12-30 12:15:44 -0500

    Hamming without length

--- haskell/hamming/src/Hamming.hs
+++ haskell/hamming/src/Hamming.hs
@@ -1,6 +1,12 @@
 module Hamming (distance) where
 
 distance :: (Eq a) => [a] -> [a] -> Maybe Integer
-distance x y = if length x /= length y
-        then Nothing
-        else Just (sum $ map (\b -> if b then 1 else 0) $ zipWith (/=) x y)
+distance = currentPlusDistanceRest 0
+
+currentPlusDistanceRest :: (Eq a) => Integer -> [a] -> [a] -> Maybe Integer
+currentPlusDistanceRest n [] [] = Just n
+currentPlusDistanceRest n [] _  = Nothing
+currentPlusDistanceRest n _  [] = Nothing
+currentPlusDistanceRest n (x:xs) (y:ys)
+        | x /= y = currentPlusDistanceRest (n + 1) xs ys
+        | x == y = currentPlusDistanceRest  n      xs ys
