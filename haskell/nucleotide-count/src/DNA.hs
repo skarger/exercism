958b11645cabbec08be8ed67e01c85f0045324e7
Stephen Karger <stephen@bridj.com>
2017-01-07 21:41:43 -0500

    working nucleotideCounts

--- haskell/nucleotide-count/src/DNA.hs
+++ haskell/nucleotide-count/src/DNA.hs
@@ -1,7 +1,6 @@
 module DNA (count, nucleotideCounts) where
 
-import Data.Map (Map)
-import Data.Either (isRight)
+import Data.Map (Map, fromList)
 
 count :: Char -> String -> Either String Int
 count c "" = Right 0
@@ -11,8 +10,12 @@ count c (n:ns)
         | c == n = fmap (+1) $ count c ns
         | c /= n = count c ns
 
-nucleotideCounts :: String -> Either String (Map Char Int)
-nucleotideCounts s = Left ""
-
 invalidNucleotide :: Char -> Bool
 invalidNucleotide c = not $ any (c ==) "ATCG"
+
+nucleotideCounts :: String -> Either String (Map Char Int)
+nucleotideCounts s = 
+        case (count 'A' s, count 'C' s, count 'G' s, count 'T' s) of
+              (Right a, Right c, Right g, Right t) ->
+                      Right $ fromList [('A', a), ('C', c), ('G', g), ('T', t)]
+              otherwise -> Left "Invalid nucleotide found in strand"
