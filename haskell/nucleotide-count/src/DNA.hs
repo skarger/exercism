2e73c4a8dcae80eb6c2696554edb2ab641aec735
Stephen Karger <stephen@bridj.com>
2017-01-07 23:27:25 -0500

    refactor again

--- haskell/nucleotide-count/src/DNA.hs
+++ haskell/nucleotide-count/src/DNA.hs
@@ -8,8 +8,8 @@ nucleotides = ['A', 'C', 'G', 'T']
 count :: Char -> String -> Either String Int
 count c "" = Right 0
 count c (n:ns)
-  | not $ elem c nucleotides = Left "Invalid nucleotide given to count"
-  | not $ elem n nucleotides = Left "Invalid nucleotide found in strand"
+  | notElem c nucleotides = Left "Invalid nucleotide given to count"
+  | notElem n nucleotides = Left "Invalid nucleotide found in strand"
   | otherwise = let incrementBy = if c == n then 1 else 0
                  in fmap (+incrementBy) $ count c ns
 
@@ -17,5 +17,5 @@ nucleotideCounts :: String -> Either String (Map Char Int)
 nucleotideCounts s = 
         case (count 'A' s, count 'C' s, count 'G' s, count 'T' s) of
           (Right a, Right c, Right g, Right t) ->
-                  Right $ fromList $ zip nucleotides [a, c, g, t]
+                  Right $ fromList $ zip "ACGT" [a, c, g, t]
           otherwise -> Left "Invalid nucleotide found in strand"
