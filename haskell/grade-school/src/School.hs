f22d688e50f6bdcb1a173271ea08af14e435fe13
Stephen Karger <stephen@bridj.com>
2017-01-15 15:14:17 -0500

    fetched grade-school

---
+++ haskell/grade-school/src/School.hs
@@ -0,0 +1,15 @@
+module School (School, add, empty, grade, sorted) where
+
+data School = Dummy
+
+add :: Int -> String -> School -> School
+add gradeNum student school = undefined
+
+empty :: School
+empty = undefined
+
+grade :: Int -> School -> [String]
+grade gradeNum school = undefined
+
+sorted :: School -> [(Int, [String])]
+sorted school = undefined
