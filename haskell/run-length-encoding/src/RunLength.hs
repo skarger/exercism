5bf0c08deb87260bd3bb99c203e060da20735ac4
Stephen Karger <stephen@bridj.com>
2017-01-15 15:08:31 -0500

    RunLength with recursion and inline helpers

--- haskell/run-length-encoding/src/RunLength.hs
+++ haskell/run-length-encoding/src/RunLength.hs
@@ -4,36 +4,16 @@ import Data.List (group, groupBy)
 import Data.Char (isDigit)
 
 decode :: String -> String
-decode s = concatMap decompress $
-                pairs . normalizeEncoded $
-                groupBy groupCharacters s
+decode "" = ""
+decode str@(c:cs)
+  | isDigit c = replicate count character ++ decode rest
+  | otherwise = c : decode cs
+  where (countString, character:rest) = span isDigit str
+        count = read countString :: Int
 
 encode :: String -> String
-encode s = concat $ zipWith (++) counts characters
-        where counts     = map characterCount $ group s
-              characters = map ((:[]) . head) $ group s
-
-
-decompress :: (String, String) -> String
-decompress (countString, characterString) = replicate count character
-        where count = read countString :: Int
-              character = head characterString
-
-pairs :: [String] -> [(String, String)]
-pairs [] = []
-pairs (x:y:zs) = (x, y):pairs zs
-
-normalizeEncoded :: [String] -> [String]
-normalizeEncoded [] = []
-normalizeEncoded [s] = ["1", s]
-normalizeEncoded (x:y:zs)
-  | isDigit (head x) = [x, y] ++ normalizeEncoded zs
-  | otherwise = ["1", x] ++ normalizeEncoded (y:zs)
-
-groupCharacters :: Char -> Char -> Bool
-groupCharacters c d = isDigit c && isDigit d
-
-characterCount :: String -> String
-characterCount s
-  | length s == 1 = ""
-  | otherwise = show $ length s
+encode str = concatMap encodeRun . group $ str
+        where encodeRun run@(c:cs) = runLength run ++ [c]
+              runLength run
+                | length run > 1 = show $ length run
+                | otherwise = ""
