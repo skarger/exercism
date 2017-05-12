55f5b3002f0715532d46ba704a3a39e04b35a30f
Stephen Karger <stephen.karger@gmail.com>
2017-05-11 22:21:40 -0400

    one liner

--- python/leap/leap.py
+++ python/leap/leap.py
@@ -1,10 +1,2 @@
 def is_leap_year(year):
-    if year % 4 == 0:
-        if year % 400 == 0:
-            return True
-        elif year % 100 == 0:
-            return False
-        else:
-            return True
-    else:
-        return False
+    return (year % 4 == 0) and (year % 100 != 0 or year % 400 == 0)
