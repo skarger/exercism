49ed795474e7aec3f6fad140e107ca3efbde5a11
Stephen Karger <stephen.karger@gmail.com>
2017-06-05 23:14:51 -0400

    revised python pangram

--- python/pangram/pangram.py
+++ python/pangram/pangram.py
@@ -4,4 +4,4 @@ import string
 
 def is_pangram(sentence):
     normalized_sentence = re.sub("[^A-Za-z]", "", sentence).lower()
-    return "".join(sorted(set(normalized_sentence))) == string.ascii_lowercase
+    return set(normalized_sentence) == set(string.ascii_lowercase)
