5aef3ff487a412949030411230532016b77edbbf
Stephen Karger <stephen.karger@gmail.com>
2017-06-05 23:16:54 -0400

    revise isogram

--- python/isogram/isogram.py
+++ python/isogram/isogram.py
@@ -3,4 +3,4 @@ import re
 
 def is_isogram(string):
     normalized = re.sub("[^A-Za-z]", "", string.lower())
-    return len(list(normalized)) == len(set(normalized))
+    return len(normalized) == len(set(normalized))
