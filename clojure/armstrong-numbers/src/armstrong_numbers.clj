c9172f36b4dc9a22dd6bdf9d3841438ccede05fa
Stephen Karger <stephen.karger@gmail.com>
2018-12-28 17:40:18 -0500

    Format

--- clojure/armstrong-numbers/src/armstrong_numbers.clj
+++ clojure/armstrong-numbers/src/armstrong_numbers.clj
@@ -2,5 +2,10 @@
 (require '[clojure.string :as str])
 
 (defn armstrong? [num]
-  (let [power (count (str num)) digits (map (fn [s] (.pow (BigInteger. s ) power)) (str/split (str num) #"") ) sum (reduce + digits)  ] (= sum num) )
+  (let [power (count (str num))
+      digits (map
+               (fn [s] (.pow (BigInteger. s) power))
+               (str/split (str num) #""))
+      sum (reduce + digits)]
+  (= sum num))
 )
