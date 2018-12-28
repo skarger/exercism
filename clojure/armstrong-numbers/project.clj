159c4a413b3087293050aa0f9e160596f35e3caa
Stephen Karger <stephen.karger@gmail.com>
2018-12-28 18:13:44 -0500

    Format with cljfmt

--- clojure/armstrong-numbers/project.clj
+++ clojure/armstrong-numbers/project.clj
@@ -1,4 +1,5 @@
 (defproject armstrong-numbers "0.1.0-SNAPSHOT"
   :description "armstrong-numbers exercise"
   :url "https://github.com/exercism/clojure/tree/master/exercises/armstrong-numbers"
-  :dependencies [[org.clojure/clojure "1.9.0"]])
+  :dependencies [[org.clojure/clojure "1.9.0"]]
+  :plugins [[lein-cljfmt "0.6.3"]])
--- clojure/armstrong-numbers/src/armstrong_numbers.clj
+++ clojure/armstrong-numbers/src/armstrong_numbers.clj
@@ -3,9 +3,8 @@
 
 (defn armstrong? [num]
   (let [power (count (str num))
-      digits (map
-               (fn [s] (.pow (BigInteger. s) power))
-               (str/split (str num) #""))
-      sum (reduce + digits)]
-  (= sum num))
-)
+        digits (map
+                (fn [s] (.pow (BigInteger. s) power))
+                (str/split (str num) #""))
+        sum (reduce + digits)]
+    (= sum num)))
