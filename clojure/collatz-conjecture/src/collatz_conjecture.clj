cdb08549c52627ae486aa8e2ea51bd62310287f4
Stephen Karger <stephen.karger@gmail.com>
2018-12-30 12:09:14 -0500

    Dasherize variable names

--- clojure/collatz-conjecture/src/collatz_conjecture.clj
+++ clojure/collatz-conjecture/src/collatz_conjecture.clj
@@ -3,6 +3,6 @@
 (defn collatz [num]
   (if (= num 1)
     0
-    (let [current_step 1
-          next_number (if (even? num) (/ num 2) (+ 1 (* 3 num)))]
-      (+ current_step (collatz next_number)))))
+    (let [current-step 1
+          next-number (if (even? num) (/ num 2) (+ 1 (* 3 num)))]
+      (+ current-step (collatz next-number)))))
