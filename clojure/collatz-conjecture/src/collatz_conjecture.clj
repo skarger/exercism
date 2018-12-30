833fbaa1bea58526fd825d2e82a3c3e147d3c9d5
Stephen Karger <stephen.karger@gmail.com>
2018-12-30 11:55:23 -0500

    Working collatz

--- clojure/collatz-conjecture/src/collatz_conjecture.clj
+++ clojure/collatz-conjecture/src/collatz_conjecture.clj
@@ -1,5 +1,7 @@
 (ns collatz-conjecture)
 
-(defn collatz [num] ;; <- arglist goes here
-  ;; your code goes here
-)
+(defn collatz [num]
+  (cond
+    (= num 1) 0
+    (even? num) (+ 1 (collatz (/ num 2)))
+    :else (+ 1 (collatz (+ 1 (* 3 num))))))
