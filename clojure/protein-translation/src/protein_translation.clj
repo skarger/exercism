f9288282e410960d4c1178404d7ae0adb8181def
Stephen Karger <stephen.karger@gmail.com>
2018-12-30 20:43:20 -0500

    Solve protein-translation

--- clojure/protein-translation/src/protein_translation.clj
+++ clojure/protein-translation/src/protein_translation.clj
@@ -1,9 +1,30 @@
 (ns protein-translation)
+(require '[clojure.string :as str])
 
-(defn translate-codon [] ;; <- arglist goes here
-      ;; your code goes here
-      )
+(defn translate-codon [codon]
+  ({"AUG" "Methionine"
+    "UUU" "Phenylalanine"
+    "UUC" "Phenylalanine"
+    "UUA" "Leucine"
+    "UUG" "Leucine"
+    "UCU" "Serine"
+    "UCC" "Serine"
+    "UCA" "Serine"
+    "UCG" "Serine"
+    "UAU" "Tyrosine"
+    "UAC" "Tyrosine"
+    "UGU" "Cysteine"
+    "UGC" "Cysteine"
+    "UGG" "Tryptophan"
+    "UAA" "STOP"
+    "UAG" "STOP"
+    "UGA" "STOP"} codon "STOP"))
 
-(defn translate-rna [] ;; <- arglist goes here
-  ;; your code goes here
-  )
+(defn convert-proteins [result nucleotides]
+  (let [protein (translate-codon (str/join (take 3 nucleotides)))]
+    (if (= "STOP" protein)
+      result
+      (convert-proteins (conj result protein) (drop 3 nucleotides)))))
+
+(defn translate-rna [rna-sequence]
+  (convert-proteins [] (str/split rna-sequence #"")))
