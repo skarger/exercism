eda60f7b23c99de9e0f5f9fdd9c2de8ebd5553fa
Stephen Karger <stephen.karger@gmail.com>
2019-01-03 20:29:04 -0500

    Progress on spiral matrix

--- clojure/spiral-matrix/src/spiral_matrix.clj
+++ clojure/spiral-matrix/src/spiral_matrix.clj
@@ -1,5 +1,16 @@
 (ns spiral-matrix)
 
-(defn spiral [n] ;; <- arglist goes here
-  ;; your code goes here
-  )
+
+;; (defn max-digits [n] (.intValue (Math/log10 n)))
+
+(defn n-numbers [n] (* n n))
+
+(defn build [n have remaining]
+  (if (= 0 (count remaining))
+    have
+    (build n (conj have (reverse (take n remaining))) (drop n remaining))))
+
+(defn spiral [n]
+  (let [numbers (range (n-numbers n) 0 -1)] (build n '() numbers)))
+
+
