37f380946e5cfdf60e6d2b57abcbe9156f182331
Stephen Karger <stephen.karger@gmail.com>
2019-01-06 00:59:10 -0500

    Refactor

--- clojure/spiral-matrix/src/spiral_matrix.clj
+++ clojure/spiral-matrix/src/spiral_matrix.clj
@@ -1,23 +1,22 @@
 (ns spiral-matrix)
 
 ;; prepares coordinates of the "outer wall" of a spiral starting at some coordinate
-(defn outer-coordinates [start-row start-col n]
-  (let [row-end (+ start-row n)
-        col-end (+ start-col n)
-        right (map vector (take n (repeat start-row)) (range start-col col-end))
-        down (map vector (range (+ start-row 1) row-end) (take (- n 1) (repeat (- col-end 1))))
-        left (map vector (take (- n 1) (repeat (- row-end 1))) (range (- col-end 2) (- start-col 1) -1))
-        up (map vector (range (- row-end 2) start-row -1) (take (- n 2) (repeat start-col)))]
+(defn outer-coordinates [start current-n]
+  (let [upper-bound (+ start current-n)
+        right (map vector (take current-n (repeat start)) (range start upper-bound))
+        down (map vector (range (+ start 1) upper-bound) (take (- current-n 1) (repeat (- upper-bound 1))))
+        left (map vector (take (- current-n 1) (repeat (- upper-bound 1))) (range (- upper-bound 2) (- start 1) -1))
+        up (map vector (range (- upper-bound 2) start -1) (take (- current-n 2) (repeat start)))]
     (concat right down left up)))
 
 ;; ordered coordinates for entire spiral
-(defn spiral-coordinates [coordinates-so-far start-row start-col current-n]
+(defn spiral-coordinates [coordinates-so-far start current-n]
   (cond (= current-n 0) coordinates-so-far
-        (= current-n 1) (conj coordinates-so-far (vector start-row start-col))
+        (= current-n 1) (conj coordinates-so-far (vector start start))
         :else
         (spiral-coordinates
-          (into [] (concat coordinates-so-far (outer-coordinates start-row start-col current-n)))
-          (+ start-row 1) (+ start-col 1) (- current-n 2))))
+          (into [] (concat coordinates-so-far (outer-coordinates start current-n)))
+          (+ start 1) (- current-n 2))))
 
 (defn preallocate-matrix [n]
   (letfn [(row [n] (into [] (take n (repeat -1))))]
@@ -29,11 +28,8 @@
 
 (defn spiral [n]
   (let [spiral-matrix (preallocate-matrix n)
-        all-coordinates (spiral-coordinates [] 0 0 n)
+        all-coordinates (spiral-coordinates [] 0 n)
         all-numbers (range 1 (+ 1 (* n n)))
         coordinates-vals (map conj all-coordinates all-numbers)
         set-at-coordinate (fn [matrix coordinate-val] (apply set-row-col matrix coordinate-val))]
-    (map seq (reduce
-               (fn [matrix coordinate-val] (set-at-coordinate matrix coordinate-val))
-               spiral-matrix
-               coordinates-vals))))
+    (map seq (reduce set-at-coordinate spiral-matrix coordinates-vals))))
