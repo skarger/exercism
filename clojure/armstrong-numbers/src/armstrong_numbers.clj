8aaf712e8558a60af38e8d60f113eed3f592f8ba
Stephen Karger <stephen.karger@gmail.com>
2019-01-12 19:09:50 -0500

    Refactor to thread last macro

--- clojure/armstrong-numbers/src/armstrong_numbers.clj
+++ clojure/armstrong-numbers/src/armstrong_numbers.clj
@@ -2,9 +2,11 @@
 (require '[clojure.string :as str])
 
 (defn armstrong? [num]
-  (let [power (count (str num))
-        digits (map
-                (fn [s] (.pow (BigInteger. s) power))
-                (str/split (str num) #""))
-        sum (reduce + digits)]
-    (= sum num)))
+  (let [power (count (str num))]
+    (->> num
+         str
+         (#(str/split % #""))
+         (map #(BigInteger. %))
+         (map #(.pow % power))
+         (reduce +)
+         (= num))))
