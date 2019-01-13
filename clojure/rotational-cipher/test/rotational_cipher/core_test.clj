ede6e67faed7196519466ce86b7b08cac2297cea
Stephen Karger <stephen.karger@gmail.com>
2019-01-12 19:30:47 -0500

    Commit fresh rotational-cipher

---
+++ clojure/rotational-cipher/test/rotational_cipher/core_test.clj
@@ -0,0 +1,7 @@
+(ns rotational-cipher.core-test
+  (:require [clojure.test :refer :all]
+            [rotational-cipher.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
